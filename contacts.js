b4a6068d055cc1033841092c1231da3a40c031f0
Nazar Dia <nazar.dia@gmail.com>
2024-04-21 15:17:51 +0200

    edited

--- contacts.js
+++ contacts.js
@@ -19,11 +19,7 @@ export async function listContacts() {
 export async function getContactById(contactId) {
   const contacts = await listContacts();
   const result = contacts.filter((f) => f.id === contactId)[0];
-  if (result.length === 0) {
-    return null;
-  } else {
-    return result;
-  }
+  return result ? result : null;
 }
 
 export async function addContact(name, email, phone) {
@@ -38,10 +34,12 @@ export async function removeContact(contactId) {
   const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === contactId);
   if (index === -1) {
+    console.log(null);
     return null;
   } else {
     const deletedData = contacts.splice(index, 1)[0];
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    console.log(deletedData);
     return deletedData;
   }
 }
